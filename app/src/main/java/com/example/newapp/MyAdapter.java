afaafa6597575674346bf84839150f39f5b08a5f
eunhwa99 <dmsghkp@naver.com>
2021-01-25 16:32:43 +0900

    listview

---
+++ app/src/main/java/com/example/newapp/MyAdapter.java
@@ -0,0 +1,72 @@
+package com.example.newapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.newapp.database.Students;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends BaseAdapter {
+    // Adapter에 추가된 데이터를 저장하기 위한 ArrayList
+    static public ArrayList<Students> studentlist=new ArrayList<Students>();
+
+    public MyAdapter(){
+
+    }
+
+    //Adapter에 사용되는 데이터의 개수
+    @Override
+    public int getCount() {
+        return studentlist.size();
+    }
+
+    // 지정한 position에 있는 데이터 리턴
+    @Override
+    public Object getItem(int position) {
+        return studentlist.get(position);
+    }
+
+    // 지정한 position에 있는 데이터와 관계된 아이템의 ID 리턴
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    //아이템 데이터 추가를 위한 함수
+    public void addItem(String name, int age, String phone, String date){
+        Students students=new Students(name, age, phone, date);
+        studentlist.add(students);
+    }
+
+    // position에 위치한 데이터를 화면에 출력하는데 사용될 View 리턴
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos=position;
+        final Context context=parent.getContext();
+
+        if(convertView==null){
+            LayoutInflater inflater=(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView=inflater.inflate(R.layout.showlist,parent, false);
+        }
+
+        // 화면에 표시될 View(Layout이 inflate 된)으로부터 위젯에 대한 참조 획득
+        TextView nameText=(TextView)convertView.findViewById(R.id.Name);
+        TextView ageText=(TextView)convertView.findViewById(R.id.Age);
+        TextView phoneText=(TextView)convertView.findViewById(R.id.Phone);
+
+        // position에 위치한 데이터 참조 획득
+        Students students=studentlist.get(position);
+
+        nameText.setText("이름: "+ students.getName());
+        ageText.setText("나이: "+students.getAge());
+        phoneText.setText("핸드폰: "+students.getPhone());
+
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/newapp/studentList.java
@@ -0,0 +1,95 @@
+package com.example.newapp;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.newapp.database.Students;
+
+import java.util.ArrayList;
+
+public class studentList extends AppCompatActivity {
+    private ListView listview;
+    private MyAdapter adapter;
+
+    ArrayList<Students> studentlist=MyAdapter.studentlist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview);
+
+        CreateView();
+
+        // listview에 꾹 눌렀을 때 이벤트 정의 ==> 삭제 기능
+        listview.setOnItemLongClickListener((new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Students students=(Students)parent.getItemAtPosition(position);
+                int flag= OnClickHandler(view);
+
+                if(flag==1){ //해당 학생 삭제
+                    studentlist.remove(position);
+                    adapter.notifyDataSetChanged();
+                }
+                else if(flag==2){ //수정
+
+                }
+                return true;
+            }
+        }));
+    }
+
+    private void CreateView(){
+        //Adapter 생성
+        adapter=new MyAdapter();
+
+        // 리스트뷰 참조 및 Apater 달기
+        listview=(ListView)findViewById(R.id.showlist);
+        listview.setAdapter(adapter);
+    }
+
+
+    private int OnClickHandler(View view){
+      AlertDialog.Builder builder=new AlertDialog.Builder(studentList.this);
+      AlertDialog alertDialog;
+        final int[] flag = {0};
+
+      builder.setTitle("알림")
+              .setMessage("수정/삭제")
+              .setPositiveButton("수정",
+              new DialogInterface.OnClickListener() {
+                  @Override
+                  public void onClick(DialogInterface dialog, int which) {
+                      flag[0] =1;
+                  }
+              });
+      builder.setNeutralButton("취소", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+              dialog.cancel();
+          }
+      });
+
+      builder.setNegativeButton("삭제", new DialogInterface.OnClickListener() {
+          @Override
+          public void onClick(DialogInterface dialog, int which) {
+              flag[0]=2;
+          }
+      });
+
+        alertDialog = builder.create();
+        alertDialog.show();
+
+        return flag[0];
+    }
+
+}
