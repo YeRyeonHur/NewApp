ba4d557a81b727e63b921da9b0e0cfade4adc4c9
Lee-Sieon <leese5phone@gmail.com>
2021-01-26 17:58:32 +0900

    timepickerdialog 추가

--- app/src/main/java/com/example/newapp/info/add_std.java
+++ app/src/main/java/com/example/newapp/info/add_std.java
@@ -1,15 +1,23 @@
 package com.example.newapp.info;
 
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.CancellationSignal;
 import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.newapp.R;
+
+import java.sql.Time;
 
 public class add_std extends AppCompatActivity {
 
@@ -27,11 +35,27 @@ public class add_std extends AppCompatActivity {
         par_phone.addTextChangedListener(new PhoneNumberFormattingTextWatcher());
     }
 
+    //등원 시간 설정 메소드
     public void clicked_days(View v){
+        TimePickerDialog timePicker;
 
+        timePicker = new TimePickerDialog(this, new TimePickerDialog.OnTimeSetListener(){
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                String setTime = hourOfDay + ":" + minute;
+                TextView tv = (TextView) v;
+                tv.setText(setTime);
+            }
+        }, 00, 00, false);
+        timePicker.setButton(TimePickerDialog.BUTTON_NEGATIVE, "삭제", new DialogInterface.OnClickListener(){
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                TextView tv = (TextView) v;
+                tv.setText("--:--");
+            }
+        });
+        timePicker.show();
     }
 
-    public void clicked_pay(View v){
 
-    }
 }
